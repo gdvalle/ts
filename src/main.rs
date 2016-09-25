3e93dda2703e8fc8bf7da539839861a2bc19b206
Greg Dallavalle <greg.dallavalle@ave81.com>
2016-09-25 03:08:17 -0500

    Add TS_FORMAT env var for configurable strftime

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ extern crate alloc_system;
 
 extern crate chrono;
 
-use std::io::{self,BufRead};
+use std::env;
+use std::io::{self, BufRead};
 
-fn echo(s: String) {
+fn echo(time_fmt: &String, s: &String) {
     let now = chrono::Local::now();
-    println!("{} {}", now.format("%Y-%m-%d %H:%M:%S%.3f").to_string(), s);
+    println!("{} {}", now.format(&time_fmt).to_string(), &s);
 }
 
 fn main() {
+    let time_fmt = match env::var("TS_FORMAT") {
+        Ok(v) => v,
+        Err(_) => String::from("%Y-%m-%d %H:%M:%S%.3f"),
+    };
+
     let stdin = io::stdin();
     loop {
         match stdin.lock().lines().next() {
-            Some(l) => echo(l.unwrap()),
+            Some(l) => echo(&time_fmt, &l.unwrap()),
             None => break,
         }
     }
 }
-
